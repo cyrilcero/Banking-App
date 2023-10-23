dd47a3b17e13a602b239c148a72c688168e02012
jeanevabejar <jeanevabejar@gmail.com>
2023-10-23 21:41:40 +0800

    signup page test

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import { Signup } from './components/Signup';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Signup/>
     </>
   )
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Signup = () => {
+  return (
+    <div>
+      <div>logo</div>
+      <div>
+        <Form>
+          <h2>Create Account</h2>
+          <label htmlFor="firstName"></label>
+          <input type="text" />
+          <label htmlFor="lastName"></label>
+          <input type="text" />
+          <label htmlFor="email"></label>
+          <input type="email" />
+          <label htmlFor="contact"></label>
+          <input type="number" />
+          <label htmlFor="password"></label>
+          <input type="password" />
+        </Form>
+      </div>
+    </div>
+  );
+};
