620b2f9271d333e539c92255f794eb19d3c46c01
Donni Rhey Noval <donnirhey4blog@gmail.com>
2023-10-27 19:42:10 -0700

    Created landing Page Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import './App.css';
+import './home.css';
+import "./components/Navbar.css";
 
-import Navbar from './components/Navbar';
+import { HomeNavBar } from './components/NavBar';
+import Home from './components/pages/Home';
+import Loans from './components/pages/Loans';
+import Cards from './components/pages/Cards';
+import Insurance from './components/pages/Insurance';
+import Investments from './components/pages/Investments';
+import PromAndRe from './components/pages/PromAndRe';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+      <HomeNavBar/>
+      <Routes>
+      <Route path='/' exact element={ <Home />}></Route>
+      <Route path='/loans' exact element={ <Loans />}></Route>
+      <Route path='/cards' exact element={ <Cards />}></Route>
+      <Route path='/insurance' exact element={ <Insurance />}></Route>
+      <Route path='/investments' exact element={ <Investments />}></Route>
+      <Route path='/promos-rewards' exact element={ <PromAndRe />}></Route>
+      </Routes>
+      </Router>
     </>
   )
-}
+} 
 
 export default App
---
+++ src/components/pages/Investments.jsx
@@ -0,0 +1,16 @@
+import '../../home.css';
+
+export default function Investments(){
+
+    const centerStyles = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center', 
+        height: '100vh',     
+      };
+
+      return (
+        <h1 style={centerStyles}>Investments</h1>
+      );
+    }
+    
