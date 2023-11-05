7f202c002fc39a6e5107de0395e74cd1bcf6fd43
cyrilcero <cyril.cero@gmail.com>
2023-11-06 03:01:24 +0800

    feat: add chart js react

---
+++ src/pages/UsersBalanceChart.jsx
@@ -0,0 +1,10 @@
+import getLocalStorage from "../utils/getLocalStorage"
+import Chart 
+
+function UsersBalanceChart() {
+  return (
+    <div>UsersBalanceChart</div>
+  )
+}
+
+export default UsersBalanceChart
