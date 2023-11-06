8979b49c92b6efdef1ee2819c40cd600a005f76e
Louise Dungca <dungca.louisechristine@gmail.com>
2023-11-06 20:16:26 +0800

    fix accountBalance bug

--- src/components/BalanceOverview.jsx
+++ src/components/BalanceOverview.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
-import { calcSpentPerUser } from '../utils/helpers';
+import { getLocalStorage } from '../utils/localStorage';
+import { calcSpentPerUser, formatCurrency } from '../utils/helpers';
 
 function BalanceOverview() {
-  const allAccounts = getLocalStorage('UserAccounts');
   const user = getLocalStorage('CurrentUser');
   const totalExpenses = calcSpentPerUser(user.email);
   const remainingBalance = user.accountBalance - totalExpenses; 
-
-  const updatedAllAccounts = allAccounts.map(account => {
-    if (account.email === user.email) {
-      account.accountBalance = remainingBalance;
-    }
-    return account;
-  });
-  
-  setLocalStorage('UserAccounts', updatedAllAccounts);
-
-  user.accountBalance = remainingBalance;
-  setLocalStorage('CurrentUser', user); 
     
   return (
     <div className='balance-overview'>
@@ -28,7 +15,7 @@ function BalanceOverview() {
       </div>
       
       <div className='account-balance'>
-        <h3 className='current-balance'><span>PHP </span>{user.accountBalance}</h3>
+        <h3 className='current-balance'><span>PHP </span>{formatCurrency(remainingBalance)}</h3>
         <span>AVAILABLE BALANCE</span>
       </div>
     </div>
