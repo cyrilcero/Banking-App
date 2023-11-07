8e36c4aed9df254c182481e6535bc0acbcee1a65
cyrilcero <cyril.cero@gmail.com>
2023-11-07 18:54:13 +0800

    fix: lift state for AdminOverviewContent

--- src/pages/AdminOverviewContent.jsx
+++ src/pages/AdminOverviewContent.jsx
@@ -3,29 +3,27 @@ import CashInForm from "../components/CashInForm";
 import ClientList from "../components/ClientList";
 import addUserImg from "../assets/admindashAddUser.png";
 import { getLocalStorage } from "../utils/localStorage";
-
+import { useState } from "react";
 
 function AdminOverviewContent() {
-  const clients = getLocalStorage('UserAccounts');;
-  const nav = useNavigate();  
-  
+  const clients = getLocalStorage("UserAccounts");
+  const [userList, setUserList] = useState(clients)
+  const nav = useNavigate();
+
   const handleClick = () => {
     nav("/admin/create-new-account");
   };
-  
+
   return (
     <>
       <div className="panel-admindash">
         <div className="content1">
-          <CashInForm />
+          <CashInForm setter={setUserList}/>
         </div>
 
         <div className="client-list-createdaccount">
           <h1>Recently Created Accounts</h1>
-          <ClientList
-            displayCount={3}
-            clients={clients}
-          />
+          <ClientList displayCount={3} clients={userList} />
         </div>
 
         <div className="content3">
@@ -42,4 +40,4 @@ function AdminOverviewContent() {
   );
 }
 
-export default AdminOverviewContent
+export default AdminOverviewContent;
