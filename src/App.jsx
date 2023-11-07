259b9c4d66225f727850b594886ef77c14264cbf
cyrilcero <cyril.cero@gmail.com>
2023-11-08 00:15:17 +0800

    ref: renamed buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { ToastContainer } from "react-toastify";
 import { HomeNavBar } from "./components/HomeNavBar";
 import { Outlet } from "react-router-dom";
 
@@ -6,18 +5,6 @@ function App() {
   return (
     <>
       <HomeNavBar />
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable={false}
-        pauseOnHover
-        theme="colored"
-      />
       <Outlet />
     </>
   );
