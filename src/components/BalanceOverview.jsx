67689ddf447a473d8b20ebd531109f114157c0e7
Louise Dungca <dungca.louisechristine@gmail.com>
2023-11-08 20:29:47 +0800

    fix expenses issue

--- src/components/BalanceOverview.jsx
+++ src/components/BalanceOverview.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
-import { calcSpentPerUser, formatCurrency } from '../utils/helpers';
+import { getLocalStorage } from '../utils/localStorage';
+import { formatCurrency } from '../utils/helpers';
 
 function BalanceOverview() {
   const user = getLocalStorage('CurrentUser');
-  const totalExpenses = calcSpentPerUser(user.email);
-  const remainingBalance = user.accountBalance - totalExpenses; 
-
-  user.newBalance = remainingBalance;
-  setLocalStorage('CurrentUser', user); 
     
   return (
     <div className='balance-overview'>
