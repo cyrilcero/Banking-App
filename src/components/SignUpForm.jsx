1e4f36a162c15d741eb308eaee7f5e6545975b2a
cyrilcero <cyril.cero@gmail.com>
2023-11-08 12:54:52 +0800

    feat: reimplemented input validation in client side account creation

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -143,7 +143,9 @@ export default function SignUpForm() {
         accountID: "",
       });
 
-      toastSuccess(`Created account for ${newUser.firstName} ${newUser.lastName}`);
+      toastSuccess(
+        `Created account for ${newUser.firstName} ${newUser.lastName}`
+      );
       navigate("/login");
     }
   }
